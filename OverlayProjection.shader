5f6df920104343dd98033949329f44adadf59d10
kkkaikk <127716159+kkkaikk@users.noreply.github.com>
2023-03-13 15:40:49 +0900

    Add files via upload

---
+++ OverlayProjection.shader
@@ -0,0 +1,87 @@
+﻿/**************************************************************
+			OmniProCam用	2019/09/21 
+**************************************************************/
+
+Shader "OmniProCam/OverlayProjection" {
+
+Properties{
+	_FrontTex("Foreground(RGB)", 2D) = "black" {}
+	_BackTex0("Background0(RGB)", 2D) = "black" {}
+	_AlphaTex0("Alpha0", 2D) = "gray" {}
+}
+
+SubShader{
+
+	Pass{
+		CGPROGRAM
+
+	struct VertInput {
+		float4 vertexPosition :		POSITION;
+		float2 texCoord :					TEXCOORD0;
+		float2 texCoordCamera0 :	TEXCOORD1;
+	};
+
+	struct FragInput {
+		float4 vertexPosition :		POSITION;
+		float2 texCoord :					TEXCOORD0;
+		float2 texCoordCamera0 :	TEXCOORD1;
+	};
+
+#pragma vertex vertexShader
+#pragma fragment fragmentShader
+//#pragma fragment fragmentShaderLerp
+#pragma target 3.0
+
+#include "UnityCG.cginc"
+
+	uniform sampler2D _FrontTex;
+	uniform sampler2D _BackTex0;
+	uniform sampler2D _AlphaTex0;					// camera image
+	uniform float _BinarizingThreshold;		// Note: 0-255 scale, so it must be converted to 0-1.0 scale
+	uniform int _imageSwitchingFlag;
+
+	/***********************************************************
+		Vertex Shader
+	 ***********************************************************/
+	FragInput vertexShader(VertInput vertexInput) {
+		FragInput vertexOutput;
+		vertexOutput.vertexPosition = UnityObjectToClipPos(vertexInput.vertexPosition);
+		vertexOutput.texCoord = MultiplyUV(UNITY_MATRIX_TEXTURE0, vertexInput.texCoord);
+		vertexOutput.texCoordCamera0 = MultiplyUV(UNITY_MATRIX_TEXTURE1, vertexInput.texCoordCamera0);
+		return vertexOutput;
+	} // vertexShader
+
+	/***********************************************************
+		Fragment Shader
+	 ***********************************************************/
+	float4 fragmentShader(FragInput fragInput) : COLOR{
+
+		if (tex2D(_AlphaTex0, fragInput.texCoordCamera0).r > _BinarizingThreshold / 255.0f) {
+
+			if (_imageSwitchingFlag) {
+				return tex2D(_FrontTex, fragInput.texCoord);
+			} else {
+				return float4(0, 0, 0, 0);
+			}
+
+
+		} else {
+
+			return float4(0, 0, 0, 0);
+			//return tex2D(_BackTex0, fragInput.texCoord);
+
+		}
+
+	} // fragmentShader
+
+
+	float4 fragmentShaderLerp(FragInput fragInput) : COLOR{
+
+		return lerp(tex2D(_FrontTex, fragInput.texCoord), tex2D(_BackTex0, fragInput.texCoord), tex2D(_AlphaTex0, fragInput.texCoordCamera0).r);
+
+	} // fragmentShaderLerp
+
+		ENDCG
+	}
+	}
+}
---
+++ OverlayProjectionTarget.cs
@@ -0,0 +1,107 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OverlayProjectionTarget : ProjectionTarget {
+  
+  protected float tick = 0;
+
+  public float imageBlinkingInterval = 0.0f;
+
+  // Update is called once per frame
+  void Update() {
+
+    /* スケールの微調整 */
+    if (!Input.GetKey(KeyCode.O)) {
+
+      /* 縦スケールの微調整 */
+      if (Input.GetKey(KeyCode.E)) {
+        scale.y += 0.1f * Time.deltaTime;
+        transform.localScale = scale;
+      } else if (Input.GetKey(KeyCode.D)) {
+        scale.y -= 0.1f * Time.deltaTime;
+        transform.localScale = scale;
+      }
+      /* 縦スケールの微調整 */
+      if (Input.GetKey(KeyCode.F)) {
+        scale.x += 0.1f * Time.deltaTime;
+        transform.localScale = scale;
+      } else if (Input.GetKey(KeyCode.A)) {
+        scale.x -= 0.1f * Time.deltaTime;
+        transform.localScale = scale;
+      }
+
+      /* オフセットの設定 */
+    } else {
+
+      /* 縦オフセットの微調整 */
+      if (Input.GetKey(KeyCode.E)) {
+        offset.y += 0.05f * Time.deltaTime;
+        transform.position = new Vector3(offset.x, 0.0f, offset.y);
+      } else if (Input.GetKey(KeyCode.D)) {
+        offset.y -= 0.05f * Time.deltaTime;
+        transform.position = new Vector3(offset.x, 0.0f, offset.y);
+      }
+
+      /* 横オフセットの微調整 */
+      if (Input.GetKey(KeyCode.F)) {
+        offset.x += 0.05f * Time.deltaTime;
+        transform.position = new Vector3(offset.x, 0.0f, offset.y);
+      } else if (Input.GetKey(KeyCode.A)) {
+        offset.x -= 0.05f * Time.deltaTime;
+        transform.position = new Vector3(offset.x, 0.0f, offset.y);
+      }
+    }
+
+    /* 縦オフセットの微調整 */
+    if (Input.GetKey(KeyCode.E)) {
+      offset.y += 0.05f * Time.deltaTime;
+      transform.position = new Vector3(offset.x, 0.0f, offset.y);
+    }
+
+    /* 2値化閾値 */
+    if (Input.GetKey(KeyCode.UpArrow)) {
+      binarizingThreshold += 1.0f;
+      if (binarizingThreshold >= 255.0f) {
+        binarizingThreshold = 255;
+      }
+      LibOmniProCam.setBinarizingThreshold(binarizingThreshold);
+      print("BinarizingThreshold: " + binarizingThreshold);
+      meshRenderer.material.SetFloat("_BinarizingThreshold", binarizingThreshold);
+    } else if (Input.GetKey(KeyCode.DownArrow)) {
+      binarizingThreshold -= 1.0f;
+      if (binarizingThreshold <= 0.0f) {
+        binarizingThreshold = 0;
+      }
+      LibOmniProCam.setBinarizingThreshold(binarizingThreshold);
+      meshRenderer.material.SetFloat("_BinarizingThreshold", binarizingThreshold);
+      print("BinarizingThreshold: " + binarizingThreshold);
+    }
+
+    /* 書き出し */
+    if (Input.GetKeyDown(KeyCode.Alpha0)) {
+      if (omniProCamDeviceManagerPtr.getCameraTextureType() == CameraTextureType.CAMERA_TEXTURE_TYPE_BINARIZED) {
+        omniProCamDeviceManagerPtr.setCameraTextureType(CameraTextureType.CAMERA_TEXTURE_TYPE_SRC);
+        print("CameraTextureType: Src");
+      } else {
+        omniProCamDeviceManagerPtr.setCameraTextureType(CameraTextureType.CAMERA_TEXTURE_TYPE_BINARIZED);
+        print("CameraTextureType: Binarized");
+      }
+    }
+
+    /* Set Camera Image to Material */
+    if (omniProCamDeviceManagerPtr.getCameraTextureType() == CameraTextureType.CAMERA_TEXTURE_TYPE_BINARIZED) {
+      meshRenderer.material.SetTexture("_AlphaTex0", omniProCamDeviceManagerPtr.getBinarizedCameraImageTexturePtr().texture);
+    } else {
+      meshRenderer.material.SetTexture("_AlphaTex0", omniProCamDeviceManagerPtr.getSrcCameraImageTexturePtr().texture);
+    }
+
+    /* Image blinking */
+    if (Input.GetKey(KeyCode.Return) || tick + imageBlinkingInterval < Time.realtimeSinceStartup) {
+      meshRenderer.material.SetInt("_imageSwitchingFlag", 1);
+      tick = Time.realtimeSinceStartup;
+    } else {
+      meshRenderer.material.SetInt("_imageSwitchingFlag", 0);
+    }
+  }
+}
